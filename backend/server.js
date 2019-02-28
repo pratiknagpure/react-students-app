6d80ce93c27e2ef20dd5add63b36ae4926f54535
pratiknagpure <pnagpure07@gmail.com>
2019-02-28 11:10:33 +0530

    removed db str

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const router = express.Router();
 
 // this is our MongoDB database
-const dbRoute = "mongodb://jelo:a9bc839993@ds151382.mlab.com:51382/jelotest";
+const dbRoute = "";
 
 // connects our back end code with the database
 mongoose.connect(dbRoute, { useNewUrlParser: true });
